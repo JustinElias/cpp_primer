8f2d033dd5e7683183beccb1398f3f3a09366532
Justin <jelias153@g.rwu.edu>
2019-01-16 20:18:46 -0600

    Add files via upload

---
+++ Section2/Exercise2.2.3_Exercises.cpp
@@ -0,0 +1,9 @@
+//2.2.3 Exercises
+
+//Which, if any, of the following names are invalid?
+
+int double = 3.1.4; //invalid
+int catch-22; //invalid 
+double Double = 3.14; //valid 
+int _; //valid 
+int 1_or_2 = 1; //valid 
---
+++ Section2/Section2.2.3_Identifiers.cpp
@@ -0,0 +1,23 @@
+//2.2.3 Identifiers
+
+int somename, someName, SomeName, SOMENAME;
+
+//usually:
+int index;
+
+//not:
+int INDEX;
+//or
+int Index;
+
+//classes begin with uppercase letter:
+class Sales_Item{
+	
+};
+
+//usually seperate two words:
+int student_loan;
+int studentLoan;
+
+//not
+int studentloan;
